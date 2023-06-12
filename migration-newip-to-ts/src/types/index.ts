95446c567e845496786356d6cf25cc671d9efc14
SogoHlopec <sogo246@gmail.com>
2023-06-13 00:00:52 +0300

    feat: add interface for response API

--- migration-newip-to-ts/src/types/index.ts
+++ migration-newip-to-ts/src/types/index.ts
@@ -1,19 +1,14 @@
 export interface Source {
     id: string;
     name: string;
-    category?: string;
-    country?: string;
-    description?: string;
-    language?: string;
-    url?: string;
 }
 
 export interface Article {
+    source: Source;
     author: string;
     content: string;
     description: string;
     publishedAt: string;
-    source: Source;
     title: string;
     url: string;
     urlToImage: string;
@@ -23,8 +18,4 @@ export interface Resp {
     articles: Article[];
     status: string;
     totalResults: number;
-}
-
-export interface Sources {
-    [index: number]: Source;
 }
