7499490c0b4fdc17a4ab1753e4939a9eec081127
SogoHlopec <sogo246@gmail.com>
2023-06-12 03:14:55 +0300

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '52e1379f16044025af9e31be8f4a8f9e', // получите свой ключ https://newsapi.org/
         });
     }
 }
