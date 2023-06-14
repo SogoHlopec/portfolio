a48e341e451417feea2059577428372ec12c0ce5
SogoHlopec <sogo246@gmail.com>
2023-06-14 14:32:55 +0300

    feat: migrate file controller

--- migration-newip-to-ts/src/components/controller/loader.ts
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -10,7 +10,7 @@ class Loader {
     }
 
     getResp(
-        { endpoint, options = {} }: { endpoint: string; options: IOptions },
+        { endpoint, options = {} }: { endpoint: string; options?: IOptions },
         callback = () => {
             console.error('No callback for GET response');
         }
