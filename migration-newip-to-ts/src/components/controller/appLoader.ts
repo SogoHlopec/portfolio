64112343443d5e955e7eaadedd1e5109fa7a0fec
SogoHlopec <sogo246@gmail.com>
2023-06-16 02:33:44 +0300

    fix: add link with proxy for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '52e1379f16044025af9e31be8f4a8f9e', // получите свой ключ https://newsapi.org/
         });
     }
