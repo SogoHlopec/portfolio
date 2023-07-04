fd74ab5c4ce454ecf133a9efe6f9683e5b7e1287
SogoHlopec <sogo246@gmail.com>
2023-07-05 01:55:13 +0300

    refactor: delete comment

--- rss-css-selectors/src/index.ts
+++ rss-css-selectors/src/index.ts
@@ -2,7 +2,7 @@ import "./index.html";
 import "./style.scss";
 import { App } from "./components/App";
 
-// alert("Пожалуйста, проверьте в последний день");
+alert("Пожалуйста, проверьте в последний день");
 
 const app = new App();
 app.start();
